11498aaf51de0ccc2713888b29579549444e3d4f
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 05:55:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ function handleResponse(event, data) {
   const repoList = `<ul>${repos
     .map(
       r =>
-        '<li><strong><a href="' +
-        r.html_url +
-        '" target="_blank">' +
-        r.name +
-        '</a></strong> - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+        `<li><strong><a href=
+         "${r.html_url}" target="_blank">
+        ${r.name}</a></strong> - <a href="#" data-repo="${
+          r.name
+        }" onclick="getCommits(this)">Get Commits</a></li>`
     )
     .join("")}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
@@ -28,7 +26,9 @@ function displayCommits() {
   const commitsList = `<ul>${commits
     .map(
       commit =>
-        `<li><strong>${commit.author.login}</strong>,${commit.commit.author.name} - ${commit.commit.message}</li>`
+        `<li><strong>${commit.author.login}</strong>,${
+          commit.commit.author.name
+        } - ${commit.commit.message}</li>`
     )
     .join("")}</ul>`;
   document.getElementById("details").innerHTML = commitsList;
