85fbd6494fa81a7d00dea52000c5fa98a5a17546
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 05:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function handleResponse(event, data) {
          "${r.html_url}" target="_blank">
         ${r.name}</a></strong> - <a href="#" data-repo="${
           r.name
-        }" data-username="${r.owner.full_name}" onclick="getCommits(this)">Get Commits</a></li>`
+        }" data-username="${r.owner.login}" onclick="getCommits(this)">Get Commits</a></li>`
     )
     .join("")}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
