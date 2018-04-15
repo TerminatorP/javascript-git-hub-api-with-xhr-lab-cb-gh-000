221d05bff3e6e3dd4a4fda4b0080cc81a2277f0f
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 05:42:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,47 @@
+function handleResponse(event, data) {
+  let repos = JSON.parse(this.responseText);
+  const repoDiv = document.getElementById("repositories");
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li><strong><a href="' +
+        r.html_url +
+        '" target="_blank">' +
+        r.name +
+        '</a></strong> - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join("")}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList;
+}
+function getRepositories() {
+  event.preventDefault();
+  let username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", handleResponse);
+  req.open("GET", `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        "<li><strong>" +
+        commit.author.login +
+        "</strong> - " +
+        commit.commit.message +
+        "</li>"
+    )
+    .join("")}</ul>`;
+  document.getElementById("details").innerHTML = commitsList;
+}
+function getCommits(el) {
+  let username = document.getElementById("username").value;
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displayCommits);
+  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`);
+  req.send();
+}
