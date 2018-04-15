cf54d18782e729a2db2b5249f5ddba4ff1c022c2
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 06:21:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function handleResponse(event, data) {
+function displayRepositories(event, data) {
   let repos = JSON.parse(this.responseText);
   const repoDiv = document.getElementById("repositories");
   const repoList = `<ul>${repos
@@ -21,7 +21,7 @@ function getRepositories() {
   event.preventDefault();
   let username = document.getElementById("username").value;
   const req = new XMLHttpRequest();
-  req.addEventListener("load", handleResponse);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", `https://api.github.com/users/${username}/repos`);
   req.send();
 }
